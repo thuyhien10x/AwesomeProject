c08422e6336426e9a64d7a0ce4786d4ea5edc9c2
Tên của bạn <you@example.com>
2024-02-10 22:15:39 +0700

    Git init

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Xin chao, toi la Hien</Text>
       <StatusBar style="auto" />
+
+      <Button
+        onPress={() => {
+          console.log('You tapped the button!');
+        }}
+        title="Press Me"
+      />
     </View>
   );
 }
